5ce391b4c70d02436ae42595b746173439b23db0
Sanjudharan <Sanjudharan>
2016-09-08 01:06:14 +0530

    EditLead

---
+++ src/main/java/pages/EditLeadPage.java
@@ -0,0 +1,47 @@
+package pages;
+
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import com.relevantcodes.extentreports.ExtentTest;
+
+import wrappers.OpentapsWrappers;
+
+public class EditLeadPage extends OpentapsWrappers {
+
+	public EditLeadPage(RemoteWebDriver driver, ExtentTest test){
+		this.driver = driver; 
+		this.test = test;
+		/**if(!verifyTitle(" opentaps CRM")){
+			reportStep("This is not Create Lead Page. Look at the SnapShot", "FAIL");
+		}**/
+	}
+	
+	public EditLeadPage selectMarketingCampaign(String data) throws InterruptedException{
+		selectVisibileTextById(prop.getProperty("EditLead.SelectMarkCampaign.Id"), data);
+		Thread.sleep(10000);
+		return this;
+	}
+	
+	public EditLeadPage clickaddMark(){
+		clickByXpath(prop.getProperty("EditLead.clickaaddMarket.xpath"));
+		return this;
+	}
+	
+	public EditLeadPage selectSource(String data) throws InterruptedException{
+		selectVisibileTextById(prop.getProperty("EditLead.SelectSource.Id"),data); //addDataSourceForm_dataSourceId
+		Thread.sleep(10000);
+		return this;
+	}
+	
+	public EditLeadPage clickaddSource(){
+		//clickByXpath("clickadd.Source.xpath");//(//input[@class="smallSubmit"])[2]
+		clickByXpath(prop.getProperty("EditLead.clickaddSource.xpath"));
+		return this;
+	}
+	public ViewLeadPage clickupdate() throws InterruptedException{
+		clickByXpath(prop.getProperty("EditLead.clickUpdate.xpath"));//(//input[@class="smallSubmit"])[1]
+		return new ViewLeadPage(driver,test);
+	}
+	
+	
+}
